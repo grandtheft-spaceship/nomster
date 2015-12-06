e8cfb62205493b500ad55130780d4093f702b49a
grandtheft-spaceship <justdizon71@gmail.com>
2015-12-06 14:32:44 +0000

    Able to send out test email to real account

--- app/mailers/notification_mailer.rb
+++ app/mailers/notification_mailer.rb
@@ -2,7 +2,7 @@ class NotificationMailer < ActionMailer::Base
   default from: "no-reply@supperclvb.com"
 
   def comment_added
-  	mail(to: "justdizon71@gmail.com",
+  	mail(to: "artisanrook@gmail.com",
   		subject: "A comment has been added to your place")
   end
 end
